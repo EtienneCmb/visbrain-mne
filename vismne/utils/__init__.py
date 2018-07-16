b800833bbf50907ed8f3ae2cc9dc91c6fe18ec2c
Etienne Combrisson <e.combrisson@gmail.com>
2018-07-15 23:01:31 -0400

    Missing functions

--- vismne/utils/__init__.py
+++ vismne/utils/__init__.py
@@ -1,4 +1,4 @@
 from .color import (Colormap, array_to_color)
 from .math import (normalize, vispy_array)
-from .mesh import (Surface, _check_units)
+from .mesh import (Surface, _check_units, smoothing_matrix, mesh_edges)
 from .system import string_types
