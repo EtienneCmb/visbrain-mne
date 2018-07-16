0eaec5a3999fa1f95a99a514f6a73d91f14c6698
Etienne Combrisson <e.combrisson@gmail.com>
2018-07-15 20:31:41 -0400

    working plot_fmri_activation example

--- examples/plot_fmri_activation.py
+++ examples/plot_fmri_activation.py
@@ -16,32 +16,17 @@ print(__doc__)
 data_path = sample.data_path()
 subjects_dir = os.path.join(data_path, 'subjects')
 
-"""
-Bring up the visualization window.
-"""
-views = ['lateral', 'ventral', 'dorsal']
-brain = Brain("fsaverage", "split", "inflated", subjects_dir=subjects_dir, views=views)
+# Bring up the visualization window.
+views = ['lateral']
+brain = Brain("fsaverage", "lh", "inflated", subjects_dir=subjects_dir)
 
-"""
-Get a path to the overlay file.
-"""
-overlay_file = "/home/etienne/Toolbox/PySurfer/examples/example_data/lh.sig.nii.gz"
+# Get a path to the overlay file.
+overlay_file = "./example_data/lh.sig.nii.gz"
 
-"""
-Display the overlay on the surface using the defaults to control thresholding
-and colorbar saturation.  These can be set through your config file.
-"""
-brain.add_overlay(overlay_file, hemi='lh')
-brain.show()
-0/0
-
-"""
-You can then turn the overlay off.
-"""
-brain.overlays["sig"].remove()
-
-"""
-Now add the overlay again, but this time with set threshold and showing only
-the positive activations.
-"""
+# Display the overlay on the surface using the defaults to control thresholding
+# and colorbar saturation.  These can be set through your config file. Set
+# threshold and showing only the positive activations.
 brain.add_overlay(overlay_file, min=5, max=20, sign="pos")
+
+# Display the brain
+brain.show()
